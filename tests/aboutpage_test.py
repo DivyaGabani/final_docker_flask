1d9f970fd8e66b1e7b8f185796323e6fbd8359f8
Divya Gabani <drg5@njit.edu>
2022-03-01 02:56:40 -0500

    updated test file

--- tests/aboutpage_test.py
+++ tests/aboutpage_test.py
@@ -1,7 +1,9 @@
-"""This test the homepage"""
+"""This test the aboutpage"""
 
 
-def test_request_example(client):
+def test_request_index(client):
     """This makes the about page"""
     response = client.get("/about")
-    assert b"Lorem ipsum" in response.data
+    assert response.status_code == 200
+    assert b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper ultricies erat vitae volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed aliquet posuere porta. Nulla sem metus, pharetra vitae ante id, feugiat lobortis velit. Donec mollis eros maximus est viverra venenatis. Donec sed lorem sem. Mauris sit amet pulvinar neque. Nulla facilisi. Vestibulum sit amet maximus eros." in response.data
+
